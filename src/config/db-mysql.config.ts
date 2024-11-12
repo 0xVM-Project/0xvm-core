ec4b67e1786ec5bb4dd9ed5ab789389af23fc858
joybreeze <165615715+joybreeze@users.noreply.github.com>
2024-11-12 12:11:15 +0800

    Fixed the issue that when loading entities from mysql, they would be loaded into sqlite entities

--- src/config/db-mysql.config.ts
+++ src/config/db-mysql.config.ts
@@ -1,5 +1,21 @@
 import { registerAs } from '@nestjs/config'
 import * as path from 'path'
+import * as glob from 'glob';
+
+/**
+ * Load entities using glob patterns while excluding specific directories
+ * @returns {string[]} List of entity file paths
+ */
+function getEntities(): string[] {
+  const baseDir = path.dirname(__dirname);
+  // Glob pattern to match all entity files
+  const allEntities = glob.sync(path.join(baseDir, '/**/*.entity{.ts,.js}'));
+  // Glob patterns to match files in directories we want to exclude
+  const excludedEntities = glob.sync(path.join(baseDir, '/**/sqlite-entities/*.entity{.ts,.js}'));
+  // Filter out excluded entities
+  const filteredEntities = allEntities.filter(entity => !excludedEntities.includes(entity));
+  return filteredEntities;
+}
 
 export default registerAs('db-mysql', () => ({
     type: 'mysql',
@@ -8,6 +24,6 @@ export default registerAs('db-mysql', () => ({
     username: process.env.DB_USERNAME || 'root',
     password: process.env.DB_PASSWORD || '',
     database: process.env.DB_DATABASE || 'test',
-    entities: [path.dirname(__dirname) + '/**/*.entity{.ts,.js}'],
+    entities: getEntities(),
     synchronize: process.env.DB_SYNCHRONIZE == 'true' ? true : false,
 }));
